37472891eae7d96f5ee41ccf052ea086fcebb18f
Miljan9602 <rakitamiljan@yahoo.com>
2025-03-12 10:36:13 +0100

    flow

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -29,8 +29,7 @@ class AuthController extends Controller
 
         Bugsnag::notifyError('Custom Error', 'Redirect: Something went wrong with user signup');
 
-        return Socialite::driver('twitter')->redirect();
-
+//        return Socialite::driver('twitter')->redirect();
 
         return response()->json([
             'status' => 'ok',
