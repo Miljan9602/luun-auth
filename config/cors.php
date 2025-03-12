c43cccb8fb94d9aef1f9cfeaa7eba24241f273f9
Miljan9602 <rakitamiljan@yahoo.com>
2025-03-12 12:47:54 +0100

    Fix

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://127.0.0.1:9134', 'http://127.0.0.1:9135', 'http://localhost:9134', 'http://localhost:9135', 'https://localhost:5174/', 'https://127.0.0.1:5174/', 'https://dragonswap-auth-front-2b222aceb76b.herokuapp.com/', 'https://dragon-swap-git-feature-xdrg-nima-enterprises.vercel.app/'],
+    'allowed_origins' => ['http://127.0.0.1:9134', 'http://127.0.0.1:9135', 'http://localhost:9134', 'http://localhost:9135', 'https://localhost:5174', 'https://127.0.0.1:5174', 'https://dragonswap-auth-front-2b222aceb76b.herokuapp.com', 'https://dragon-swap-git-feature-xdrg-nima-enterprises.vercel.app'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
